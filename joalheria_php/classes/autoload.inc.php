cdeb0328f06e60064016b29514af50f83a6916eb
JOAO PAULO RODRIGUES DE MELO <98536666+PauloRodri29@users.noreply.github.com>
2023-01-17 10:32:24 -0300

    Delete joalheria_php directory

--- joalheria_php/classes/autoload.inc.php
+++
@@ -1,6 +0,0 @@
-<?php
-function meu_autoloader($classe) {
-	include_once "{$classe}.class.php";
-}
-spl_autoload_register('meu_autoloader');
-?>
--- joalheria_php/configs/config.inc.php
+++
@@ -1,6 +0,0 @@
-<?php
-define('HOST', 'localhost');
-define('DBNAME', 'joalheria');
-define('USER', 'root');
-define('PASS', 'password');
-?>
